4aec4a9a40802fbc9dce7f9c51721badf9f6edd7
Shrey Raj <119098647+Shrey-Raj@users.noreply.github.com>
2023-10-01 15:39:50 +0530

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -1,10 +1,6 @@
-# dag codes
 from datetime import datetime, timedelta
-
 from airflow import DAG
-
 from airflow.operators.python import PythonOperator
-
 from training import training
 from evaluation import evaluate
 
@@ -13,13 +9,13 @@ default_args = {
     'retries': 6,
     'retry_delay': timedelta(minutes=4)
 }
-with DAG(
-        dag_id='ml_project',
-        default_args=default_args,
-        description='ML project',
-        schedule_interval='* * * * *',
-        start_date=datetime(2020, 1, 6)
-) as dag:
+
+with DAG('ml_project',
+         default_args=default_args,
+         description='ML project',
+         schedule_interval='* * * * *',
+         start_date=datetime(2020, 1, 6)) as dag:
+    
     train_model = PythonOperator(
         task_id='train_model',
         python_callable=training
@@ -28,7 +24,7 @@ with DAG(
     evaluate_model = PythonOperator(
         task_id='evaluate_model',
         python_callable=evaluate
-    ) 
+    )
 
- 
     train_model >> evaluate_model
+             
